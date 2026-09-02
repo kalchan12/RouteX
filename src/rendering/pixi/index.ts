4db80894bb6ff741d56f12e9fc42e3a07c26c76d
kalchan12 <kalpsycho51@gmail.com>
2026-09-02 21:05:06 +0300

    refactor(rendering): remove PixiJS rendering pipeline completely

--- src/rendering/pixi/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './roadRenderer';
-export * from './nodeRenderer';
-export * from './vehicleRenderer';
-export * from './trafficLightRenderer';
-export * from './simulationRenderer';
